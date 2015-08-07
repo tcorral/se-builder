2768dcefac244fa511a818f4cbd18dcc3f73fc00
Zarkonnen <david.stark@zarkonnen.com>
2015-08-07 15:49:17 +0200

    Code style.

--- seleniumbuilder/chrome/content/html/js/builder/suite2.js
+++ seleniumbuilder/chrome/content/html/js/builder/suite2.js
@@ -101,10 +101,11 @@ builder.suite.getScriptNames = function() {
     var script = builder.suite.scripts[i];
     var name = "[" + _t('untitled_script') + " " + (i + 1) + "]";
     if (script.path) {
-      if (script.path.path.indexOf("/") > - 1) //unix path
+      if (script.path.path.indexOf("/") != - 1) {
         name = script.path.path.split("/");
-      else //windows path
-        name = script.path.path.split("\\");
+      } else {
+        name = script.path.path.split("\\"); // Maybe it's a windows path?
+      }
       name = name[name.length - 1].split(".")[0];
     }
     if (script.exportpath) {
